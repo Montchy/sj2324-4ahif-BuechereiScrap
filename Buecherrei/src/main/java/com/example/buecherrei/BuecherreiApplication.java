d78aade2a99fbd80a048465ddbf03b571cbf413a
Amelie Roggenbauer <rog22257@spengergasse.at>
2023-12-04 10:45:43 +0100

    Update

---
+++ Buecherrei/src/main/java/com/example/buecherrei/BuecherreiApplication.java
@@ -0,0 +1,13 @@
+package com.example.buecherrei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuecherreiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuecherreiApplication.class, args);
+    }
+
+}
---
+++ Buecherrei/src/test/java/com/example/buecherrei/BuecherreiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buecherrei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuecherreiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
